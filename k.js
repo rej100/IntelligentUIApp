4d611e1b0f555be686b8224884945d1485ab76c7
AleksanderMasojc <olekmasojc1@gmail.com>
2024-10-05 23:24:55 +0200

    final without ai

--- k.js
+++ k.js
@@ -1,6 +1,7 @@
 let id = 0;
 let categories = ['self-study', 'work', 'transport', 'workout', 'lecture', 'tutorial', 'lab', 'project meeting', 'other']
 let intervals = [];
+let tasksArray = [];
 
 
 function storeArrayInLocalStorage() 
@@ -18,6 +19,20 @@ function downloadArrayOnLoad()
         storedCategories = localStorage.getItem('categories');
     }
     categories = JSON.parse(storedCategories);
+
+    if (localStorage.getItem('tasks')) 
+    {
+        tasksArray = JSON.parse(localStorage.getItem('tasks'));
+
+        console.log("downloadArrayOnLoad")
+        //console.log(tasksArray)
+
+        tasksArray.forEach(task => 
+        {
+            intervals.push([task.start, task.end]);
+            renderTask(task)
+        });
+    }
 }
 
 
@@ -27,15 +42,22 @@ function addElementToArray(newElement)
     {
         categories.push(newElement);
         localStorage.setItem('categories', JSON.stringify(categories));
-        // adsadas
-        console.log(categories);
     }
 }
 
 // Event listener for page load
-window.onload = function() 
+window.onload = function()
 {
     downloadArrayOnLoad();
+
+    if(tasksArray.length == 0)
+    {
+    addTask(8.5, 13, 'Introduction to AI', 'tutorial');
+    addTask(13.5, 15.5, 'Computer Netorks', 'lecture');
+    addTask(16, 18, 'IUI', 'tutorial');
+    }
+
+    console.log(tasksArray);
 }
 
 const tasks = document.getElementById("t")
@@ -45,45 +67,57 @@ for (let i = 0; i < tasks.children.length; i++)
     element.style.top = (((i*100)/25) + 0.8).toString() + "%";
 }
 
-function deleteTask(taskElement, start, end) {
+function deleteTask(div, task) 
+{
+    intervals = intervals.filter(interval => interval[0] !== task.start || interval[1] !== task.end);
+    tasksArray = tasksArray.filter( t => t.start !== task.start || t.end !== task.end || t.name !== task.name || t.category !== task.category);
 
-    taskElement.remove();
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+    console.log(JSON.parse(localStorage.getItem('tasks')))
 
-    intervals = intervals.filter(interval => interval[0] !== start || interval[1] !== end);
-    console.log(`Task from ${start} to ${end} deleted.`);
+    div.remove();
 }
+
+let day = new Date().getDay(); // Gets the current day (0 = Sunday, 6 = Saturday)
 
 function addTask(start, end, name, category)
 {
-    if(end > 24) 
+    if (end > 24)
     {
-        alert("You cannot schedule tasks which end tommorow");
+        alert("You cannot schedule tasks which end tomorrow");
         return;
     }
 
-    if(hasOverlap([start, end])) 
+    if (hasOverlap([start, end]))
     {
         alert("You cannot schedule 2 activities at the same time");
         return;
-    }   
+    }
 
-    let div = document.createElement('div');
-    div.className = 'task';
-    div.style.top = (((start*100)/25) + 0.8).toString() + "%";
-    div.style.bottom = (100 - (((end*100)/25) + 0.8)).toString() + "%";
-    div.innerHTML =  "<span> " + name + " ("+ category + ")" +" </span>";
+    let task = { start: start, end: end, name: name, category: category, day: day };
 
-    tasks.append(div);
+    renderTask(task);
+    console.log(task)
+    tasksArray.push(task);
     intervals.push([start, end]); 
 
-    div.addEventListener('click', function () {
-        deleteTask(div, start, end);
-    });
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
 }
 
-addTask(8.5, 13, 'Introduction to AI', 'tutorial');
-addTask(13.5, 15.5, 'Computer Netorks', 'lecture');
-addTask(16, 18, 'IUI', 'tutorial');
+function renderTask(task)
+{
+    let div = document.createElement('div');
+    div.className = 'task';
+    div.style.top = (((task.start * 100) / 25) + 0.8).toString() + "%";
+    div.style.bottom = (100 - (((task.end * 100) / 25) + 0.8)).toString() + "%";
+    div.innerHTML = "<span> " + task.name + " (" + task.category + ") </span>";
+    div.addEventListener('click', function ()
+{
+        deleteTask(div, task);
+    });
+
+    tasks.append(div);
+}
 
 function cancelWindow(s)
 {
@@ -185,44 +219,14 @@ function addFormData(div)
     }
 }
 
-function hasOverlap(newTask) 
+function hasOverlap(newInterval) 
 {
-    if(intervals.length === 0) return false;
-    intervals.sort((a, b) => a[0] - b[0]);
-
-    function bs()
+    for (let interval of intervals)
     {
-        let left = 0;
-        let right = intervals.length;
-
-        while (left < right) 
+        if (Math.max(interval[0], newInterval[0]) < Math.min(interval[1], newInterval[1]))
         {
-            let mid = Math.floor((left + right) / 2);
-
-            if (intervals[mid][0] > newTask[0]) 
-            {
-                right = mid - 1;
-            } 
-            else 
-            {
-                left = mid + 1;
-            }
+            return true;
         }
-
-        return left - 1; // position of the "left" interval
-    };
-
-    let pos = bs();
-
-    if (pos >= 0 && intervals[pos][1] > newTask[0]) 
-    {
-        return true;
     }
-
-    if (pos + 1 < intervals.length && intervals[pos + 1][0] < newTask[1]) 
-    {
-        return true;
-    }
-
     return false;
 }
